c01739b48f9d8d2fa7f590dda60b1b6923a8fbb8
Theogor0 <theoberta@gmail.com>
2022-01-27 22:58:20 -0300

    Desafio 3 - Landing Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer message="Hola desde ItemListContainer"/>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,15 @@
+import CartWidget from "./CartWidget"
+
 const NavBar = (props) => {
   return <header>
-    <nav className="flex justify-between items-center border-b-2 border-gray-100 bg-teal-700 text-white py-6 px-4 md:justify-start md:space-x-10">
+    <nav className="flex justify-between items-center border-b-2 border-gray-100 bg-teal-700 text-white py-6 px-4">
       <h2 className="text-xl">Caf&eacute; de tus ojos</h2>
       <ul className="flex gap-6 justify-self-center">
-        <li><a className="hover:underline" href="/">Inicio</a></li>
-        <li><a className="hover:underline" href="/">Nuestros Cafes</a></li>
-        <li><a className="hover:underline" href="/">¿Quienes somos?</a></li>
+        <li><a className="hover:underline" href="/">Colombiano</a></li>
+        <li><a className="hover:underline" href="/">Venezolano</a></li>
+        <li><a className="hover:underline" href="/">Producción Nacional</a></li>
       </ul>
+      <CartWidget />
     </nav>
   </header>
 }
